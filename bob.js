002557f334201259844221e97ad315704ea0842e
AnantSharma0410 <72499139+AnantSharma0410@users.noreply.github.com>
2021-01-09 23:01:24 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -1,8 +1,8 @@
 class bob {
     constructor(x, y, radius) {
       var options = {
-        'restitution':0.8,
-        'friction':1.0,
+        'restitution':1.2,
+        'friction':0.0,
         'density':1.0
           
       }
